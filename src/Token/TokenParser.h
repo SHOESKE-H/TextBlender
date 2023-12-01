0201bced761c47e62423c8fb243a1b53f51457a0
shoeske-osu <fedilatrach202@gmail.com>
2023-12-01 16:38:27 +0100

    Fix TokenParser always going out-of-range
    The TokenParser always went out-of-range. This is due to the fact, that
    in it's pointer incrementing member functions, it was checked if the
    resulting increment would make the pointer out-of-range. This makes
    parsing impossible, since the member function to check if the
    pointer is out of range will be useless. Remove the check.

--- src/Token/TokenParser.h
+++ src/Token/TokenParser.h
@@ -20,7 +20,6 @@ public:
     size_t incPtr()
     {
         m_readPtr++;
-        throwIfOutOfRange();
 
         return m_readPtr;
     }
@@ -37,7 +36,6 @@ public:
     size_t ptrInc()
     {
         m_readPtr++;
-        throwIfOutOfRange();
 
         return m_readPtr - 1;
     }
